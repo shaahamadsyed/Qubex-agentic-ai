e4e2e0a32b88e6a0109539e095235f6c82540034
shaahamad-syed <shaahamadsyed@gmail.com>
2026-01-28 19:47:50 +0530

    PreFlight add up

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,20 +1,40 @@
-import axios from 'axios';
+// import axios from 'axios';
+
+// const api = axios.create({
+//     baseURL: import.meta.env.VITE_API_URL,
+// });
+
+// // Add a request interceptor to add the auth token to headers
+// api.interceptors.request.use(
+//     (config) => {
+//         const token = localStorage.getItem('token');
+//         if (token) {
+//             config.headers.Authorization = `Bearer ${token}`;
+//         }
+//         return config;
+//     },
+//     (error) => Promise.reject(error)
+// );
+
+// export default api;
+import axios from "axios";
 
 const api = axios.create({
-    // baseURL: 'http://localhost:8000',
-    baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://qubex-backend.onrender.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-// Add a request interceptor to add the auth token to headers
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => Promise.reject(error)
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
 export default api;
